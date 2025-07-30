0d28e52d707bba2ca436966cddff14102c97e115
haein Oh <badarangdev@gmail.com>
2025-07-30 16:57:09 +0900

    Update: PaymentRequestRepository base interface
    Changed PaymentRequestRepository to extend BaseTimeRepository instead of BaseCodeRepository to align with updated repository structure.

--- src/main/java/profect/eatcloud/Domain/Payment/Repository/PaymentRequestRepository.java
+++ src/main/java/profect/eatcloud/Domain/Payment/Repository/PaymentRequestRepository.java
@@ -3,11 +3,11 @@ package profect.eatcloud.Domain.Payment.Repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import profect.eatcloud.Domain.Payment.Entity.PaymentRequest;
-import profect.eatcloud.Global.TimeData.BaseCodeRepository;
+import profect.eatcloud.Global.TimeData.BaseTimeRepository;
 
 import java.util.UUID;
 import java.util.Optional;
 
 @Repository
-public interface PaymentRequestRepository extends BaseCodeRepository<PaymentRequest, UUID> {
+public interface PaymentRequestRepository extends BaseTimeRepository<PaymentRequest, UUID> {
 }
