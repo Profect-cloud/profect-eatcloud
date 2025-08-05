65fb15908d2871912b687aadb1d5f44606fa6ccf
홍성문 <2020041412@hanyang.ac.kr>
2025-08-05 16:44:36 +0900

    fix : create entity error by id

--- src/main/java/profect/eatcloud/domain/admin/entity/ManagerStoreApplication.java
+++ src/main/java/profect/eatcloud/domain/admin/entity/ManagerStoreApplication.java
@@ -4,6 +4,8 @@ import java.util.UUID;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
@@ -24,6 +26,7 @@ public class ManagerStoreApplication extends BaseTimeEntity {
 
 	@Id
 	@Column(name = "application_id", nullable = false)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private UUID applicationId;
 
 	// Manager 신청 정보
